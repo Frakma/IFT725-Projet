409936d832f2cf8ffa5c5fef580ff805b72703e0
Léo Pécault <leopec@hotmail.fr>
2020-04-06 16:30:20 +0200

    j'avais oublié de push le modeltrainer

--- training/ModelTrainer.py
+++ training/ModelTrainer.py
@@ -1,9 +1,36 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 
 class ModelTrainer(object):
     def __init__(self, model, data):
         self.model = model
         self.data = data
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
     def train(self, num_epochs):
-        pass
+        for epoch in range(num_epochs):  # loop over the dataset multiple times
+
+            running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:    # print every 2000 mini-batches
+                    print('[%d, %5d] loss: %.3f' %
+                        (epoch + 1, i + 1, running_loss / 2000))
+                    running_loss = 0.0
+
+        print('Finished Training')
