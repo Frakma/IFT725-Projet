95157765de1015fef0c030d9a902f277cfe5a5b0
Ch@ngeUP2019 <ikram20192019>
2020-04-06 13:38:42 -0400

    train.py

--- training/ModelTrainer.py
+++ training/ModelTrainer.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 import torch.optim as optim
 
 class ModelTrainer(object):
-    def __init__(self, model, data):
+    def __init__(self, model, data_train, data_test):        
         self.model = model
-        self.data = data
+        self.data_train = data_train
+        self.data_test=data_test
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
@@ -13,9 +14,9 @@ class ModelTrainer(object):
         for epoch in range(num_epochs):  # loop over the dataset multiple times
 
             running_loss = 0.0
-            for i, data in enumerate(trainloader, 0):
-                # get the inputs; data is a list of [inputs, labels]
-                inputs, labels = data
+            for i, data_train in enumerate(trainloader, 0):
+                # get the inputs; data_train is a list of [inputs, labels]
+                inputs, labels = data_train
 
                 # zero the parameter gradients
                 self.optimizer.zero_grad()
@@ -34,3 +35,42 @@ class ModelTrainer(object):
                     running_loss = 0.0
 
         print('Finished Training')
+
+    def evaluate_on_test_set(self):
+        """
+        Evaluer le modele sur l'ensemble de test
+        retourne:
+            Accuracy du model sur l'ensemble de test
+        """
+        test_loader = self.data_test
+        accuracies = 0
+        with torch.no_grad():
+            for data in test_loader:
+                test_inputs, test_labels = data[0].to(self.device), data[1].to(self.device)
+                test_outputs = self.model(test_inputs)
+                accuracies += self.accuracy(test_outputs, test_labels)
+        print("Accuracy sur l'ensemble de test: {:05.3f} %".format(100 * accuracies / len(test_loader)))
+
+
+
+# TP3
+def optimizer_setup(optimizer_class: Type[torch.optim.Optimizer], **hyperparameters) -> \
+        Callable[[torch.nn.Module], torch.optim.Optimizer]:
+    """
+    Creates a factory method that can instanciate optimizer_class with the given
+    hyperparameters.
+
+    Why this? torch.optim.Optimizer takes the model's parameters as an argument.
+    Thus we cannot pass an Optimizer to the CNNBase constructor.
+
+    Args:
+        optimizer_class: optimizer used to train the model
+        **hyperparameters: hyperparameters for the model
+        Returns:
+            function to setup the optimizer
+    """
+
+    def f(model):
+        return optimizer_class(model.parameters(), **hyperparameters)
+
+    return f
