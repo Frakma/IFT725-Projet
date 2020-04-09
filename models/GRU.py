044a0461579a0d63cd1811a2e84efac4bf2e9aea
sahourahh <sahar@DESKTOP-NCETO4N.localdomain>
2020-04-09 05:49:20 +0200

    GRU modified still have to be tested

--- models/GRU.py
+++ models/GRU.py
@@ -3,16 +3,17 @@ import torch.nn as nn
 
 #https://blog.floydhub.com/gru-with-pytorch/
 class GRU(nn.Module):
-    def __init__(self, input_dim=1, hidden_dim=100, output_dim=1,  drop_prob=0.2):
-        super(GRU, self).__init__()
-        self.hidden_dim = hidden_dim
-        #self.n_layers = n_layers
+    def __init__(self, input_dim=500, hidden_layer_size=100, output_size=100):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
         
-        self.gru = nn.GRU(input_dim, hidden_dim,  batch_first=True, dropout=drop_prob)
-        self.fc = nn.Linear(hidden_dim, output_dim)
-        self.relu = nn.ReLU()
-        
-    def forward(self, x, h):
-        out, h = self.gru(x, h)
-        out = self.fc(self.relu(out[:,-1]))
-        return out, h
+        self.gru = nn.GRU(input_dim, hidden_layer_size)
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+        #self.relu = nn.ReLU()
+        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
+                            torch.zeros(1,1,self.hidden_layer_size))
+    
+    def forward(self, input_seq):
+        gru_out, self.hidden_cell = self.gru(input_seq.view(len(input_seq), 1, -1), self.hidden_cell)
+        predictions = self.linear(gru_out.view(len(input_seq), -1))
+        return predictions
