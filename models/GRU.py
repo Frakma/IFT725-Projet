971396cb7ad09d8c05ac0e499171773134715f12
sahourahh <sahar@DESKTOP-NCETO4N.localdomain>
2020-04-09 08:08:58 +0200

    GRU done

--- models/GRU.py
+++ models/GRU.py
@@ -3,17 +3,14 @@ import torch.nn as nn
 
 #https://blog.floydhub.com/gru-with-pytorch/
 class GRU(nn.Module):
-    def __init__(self, input_dim=500, hidden_layer_size=100, output_size=100):
+    def __init__(self, input_dim=500, hidden_dim=100, output_size=100):
         super().__init__()
-        self.hidden_layer_size = hidden_layer_size
+        self.hidden_layer_size = hidden_dim
         
-        self.gru = nn.GRU(input_dim, hidden_layer_size)
-        self.linear = nn.Linear(hidden_layer_size, output_size)
-        #self.relu = nn.ReLU()
-        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
-                            torch.zeros(1,1,self.hidden_layer_size))
-    
+        self.gru = nn.GRU(input_dim, hidden_dim, num_layers=2)
+        self.linear = nn.Linear(hidden_dim, output_size)
+
     def forward(self, input_seq):
-        gru_out, self.hidden_cell = self.gru(input_seq.view(len(input_seq), 1, -1), self.hidden_cell)
+        gru_out,_ = self.gru(input_seq.view(len(input_seq), 1, -1))
         predictions = self.linear(gru_out.view(len(input_seq), -1))
         return predictions
