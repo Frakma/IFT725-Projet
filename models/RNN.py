025cb80e27a09cade001fd2193395040a226eded
ikramGit <ikram@DESKTOP-U6HP0HQ.localdomain>
2020-04-11 12:04:42 -0400

    modify RNN

--- models/RNN.py
+++ models/RNN.py
@@ -1,22 +1,15 @@
 import torch
 import torch.nn as nn
 
-#https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
 
 class RNN(nn.Module):
     def __init__(self, input_dim=500, hidden_dim=300, output_dim=100):
         super().__init__()
-
-        self.hidden_dim = hidden_dim
-
-        self.i2h = nn.Linear(input_dim + hidden_dim, hidden_dim)
-        self.i2o = nn.Linear(input_dim + hidden_dim, output_dim)
-        self.softmax = nn.LogSoftmax(dim=1)
+        self.hidden_layer_size = hidden_dim
+        self.rnn = nn.RNN(input_dim, hidden_dim, num_layers=2)
+        self.linear = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, input_seq):
-        hidden=torch.zeros(input_seq.shape[0], self.hidden_dim)
-        combined = torch.cat((input_seq, hidden), 1)
-        hidden = self.i2h(combined)
-        output = self.i2o(combined)
-        output = self.softmax(output)
-        return output
+        rnn_out, _ = self.rnn(input_seq.view(len(input_seq), 1, -1))
+        predictions = self.linear(rnn_out.view(len(input_seq), -1))
+        return predictions
